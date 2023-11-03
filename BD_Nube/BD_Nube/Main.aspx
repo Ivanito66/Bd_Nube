eb31dd819cf0ef3ea6b2261cb7c2060afd971ffb
BySaF6 <samuelarandafernandez6@gmail.com>
2023-11-03 10:14:09 +0100

    Añadido codigo

--- BD_Nube/BD_Nube/Main.aspx
+++ BD_Nube/BD_Nube/Main.aspx
@@ -1,16 +1,116 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="factura2.aspx.cs" Inherits="factura2" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Facturas</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+
+        header {
+            background-color: #0074D9;
+            color: white;
+            padding: 20px;
+        }
+
+        h1 {
+            margin: 0;
+            font-size: 36px;
+        }
+
+        .container {
+            display: flex;
+            align-items: center;
+            padding: 20px;
+            justify-content: space-between;
+        }
+
+        .filters {
+            display: flex;
+        }
+
+        .filter-select {
+            width: 150px;
+            padding: 5px;
+            font-size: 14px; 
+            margin-right: 10px; 
+        }
+
+        .buttons {
+            text-align: right;
+        }
+
+        .btn {
+            background-color: #0074D9;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-left: 10px;
+        }
+
+        .btn:hover {
+            background-color: #0056b3;
+        }
+
+        .gridview {
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+        }
+
+        .gridview th, .gridview td {
+            padding: 10px;
+            border: 1px solid #ccc;
+        }
+
+        .gridview th {
+            background-color: #0074D9;
+            color: white;
+        }
+
+        /* Estilo para filas impares (blanco) */
+        .gridview tr:nth-child(odd) {
+            background-color: #fff;
+        }
+
+        /* Estilo para filas pares (gris) */
+        .gridview tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <header>
+            <h1>Facturas</h1>
+        </header>
+        <div class="container">
+            <div class="filters">
+                <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="FiltrarFacturas" AutoPostBack="true" CssClass="filter-select">
+                </asp:DropDownList>
+                <asp:DropDownList ID="DropDownList2" runat="server" OnSelectedIndexChanged="FiltrarFacturas" AutoPostBack="true" CssClass="filter-select">
+                </asp:DropDownList>
+                <asp:Button ID="btnResetFiltros" runat="server" Text="Restablecer Filtros" OnClick="ResetFiltros" CssClass="btn" />
+            </div>
+            <div class="buttons">
+                <asp:Button ID="btnExport" runat="server" Text="Descargar" OnClick="ExportXLS" CssClass="btn" />
+            </div>
         </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true" CssClass="gridview">
+        </asp:GridView>
     </form>
 </body>
 </html>
--- BD_Nube/BD_Nube/Main.aspx.cs
+++ BD_Nube/BD_Nube/Main.aspx.cs
@@ -1,14 +1,201 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
-public partial class Main : System.Web.UI.Page
+/**
+ * Pre: Este programa es un formulario web que muestra una tabla con datos de facturas almacenadas en un XML.
+ * 
+ * Pro: Utiliza unos filtros para que el usuario pueda filtar las facturas por dos campos.
+ * También contiene dos botones, uno resetea los filtros para que vuelvan a aparecer todas las facturas
+ * y el otro permite exportar las facturas que aparecen en pantalla a formato XLS.
+ * 
+ * 16/10/2023
+ */
+public class Factura
 {
+    public string ID { get; set; }
+    public string FechaFactura { get; set; }
+    public string CIFCliente { get; set; }
+    public string NombreCliente { get; set; }
+    public decimal Importe { get; set; }
+    public decimal ImporteIVA { get; set; }
+    public string Moneda { get; set; }
+    public string FechaCobro { get; set; }
+    public string Estado { get; set; }
+}
+
+public partial class factura2 : System.Web.UI.Page
+{
+    List<Factura> facturas = new List<Factura>();
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            // Cargar el archivo XML de facturas
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("/App_Data/facturas.xml"));
 
+            // Recorrer los nodos del archivo XML y crear objetos Factura
+            foreach (XmlNode node in xmlDoc.SelectNodes("/Facturas/Factura"))
+            {
+                Factura factura = new Factura
+                {
+                    ID = node.Attributes["ID"].Value,
+                    FechaFactura = node.Attributes["FechaFactura"].Value,
+                    CIFCliente = node.Attributes["CIFCliente"].Value,
+                    NombreCliente = node.Attributes["NombreCliente"].Value,
+                    Importe = Convert.ToDecimal(node.Attributes["Importe"].Value),
+                    ImporteIVA = Convert.ToDecimal(node.Attributes["ImporteIVA"].Value),
+                    Moneda = node.Attributes["Moneda"].Value,
+                    FechaCobro = node.Attributes["FechaCobro"].Value,
+                    Estado = node.Attributes["Estado"].Value
+                };
+
+                facturas.Add(factura);
+            }
+
+            // Obtener opciones únicas de estados y monedas
+            var estados = facturas.Select(f => f.Estado).Distinct().ToList();
+            var moneda = facturas.Select(f => f.Moneda).Distinct().ToList();
+
+            // Llenar los DropDownList con las opciones de estados y monedas
+            DropDownList1.DataSource = estados;
+            DropDownList1.DataBind();
+            DropDownList2.DataSource = moneda;
+            DropDownList2.DataBind();
+
+            // Establecer el origen de datos del GridView con todas las facturas y enlazar
+            GridView1.DataSource = facturas;
+            GridView1.DataBind();
+        }
+    }
+
+    protected void FiltrarFacturas(object sender, EventArgs e)
+    {
+        // Obtener los valores seleccionados en los filtros de estado y moneda
+        string estado = DropDownList1.SelectedValue;
+        string moneda = DropDownList2.SelectedValue;
+
+        // Cargar el archivo XML de facturas
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Server.MapPath("/App_Data/facturas.xml"));
+
+        // Crear una lista para almacenar facturas filtradas
+        List<Factura> facturasFiltradas = new List<Factura>();
+
+        // Recorrer los nodos del archivo XML y aplicar filtros
+        foreach (XmlNode node in xmlDoc.SelectNodes("/Facturas/Factura"))
+        {
+            string estadoFactura = node.Attributes["Estado"].Value;
+            string monedaFactura = node.Attributes["Moneda"].Value;
+
+            // Aplicar filtros según los valores seleccionados
+            if ((string.IsNullOrEmpty(estado) || estadoFactura == estado) &&
+                (string.IsNullOrEmpty(moneda) || monedaFactura == moneda))
+            {
+                Factura factura = new Factura
+                {
+                    ID = node.Attributes["ID"].Value,
+                    FechaFactura = node.Attributes["FechaFactura"].Value,
+                    CIFCliente = node.Attributes["CIFCliente"].Value,
+                    NombreCliente = node.Attributes["NombreCliente"].Value,
+                    Importe = Convert.ToDecimal(node.Attributes["Importe"].Value),
+                    ImporteIVA = Convert.ToDecimal(node.Attributes["ImporteIVA"].Value),
+                    Moneda = monedaFactura,
+                    FechaCobro = node.Attributes["FechaCobro"].Value,
+                    Estado = estadoFactura
+                };
+
+                facturasFiltradas.Add(factura);
+            }
+        }
+
+        // Establecer el origen de datos del GridView con las facturas filtradas y enlazar
+        GridView1.DataSource = facturasFiltradas;
+        GridView1.DataBind();
+    }
+
+    protected void ResetFiltros(object sender, EventArgs e)
+    {
+        // Restablecer los filtros cargando todas las facturas nuevamente
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Server.MapPath("/App_Data/facturas.xml"));
+
+        facturas.Clear();
+
+        foreach (XmlNode node in xmlDoc.SelectNodes("/Facturas/Factura"))
+        {
+            Factura factura = new Factura
+            {
+                ID = node.Attributes["ID"].Value,
+                FechaFactura = node.Attributes["FechaFactura"].Value,
+                CIFCliente = node.Attributes["CIFCliente"].Value,
+                NombreCliente = node.Attributes["NombreCliente"].Value,
+                Importe = Convert.ToDecimal(node.Attributes["Importe"].Value),
+                ImporteIVA = Convert.ToDecimal(node.Attributes["ImporteIVA"].Value),
+                Moneda = node.Attributes["Moneda"].Value,
+                FechaCobro = node.Attributes["FechaCobro"].Value,
+                Estado = node.Attributes["Estado"].Value
+            };
+
+            facturas.Add(factura);
+        }
+
+        // Establecer el origen de datos del GridView con todas las facturas y enlazar
+        GridView1.DataSource = facturas;
+        GridView1.DataBind();
+    }
+
+    protected void ExportXLS(object sender, EventArgs e)
+    {
+        // Crear un archivo XLS en memoria y establecer las cabeceras para descargar
+        Response.Clear();
+        Response.AddHeader("content-disposition", "attachment;filename=Facturas.xls");
+        Response.ContentType = "application/ms-excel";
+
+        // Escribir el contenido del archivo XLS
+        Response.Write("<html xmlns:x='urn:schemas-microsoft-com:office:excel'>");
+        Response.Write("<head><style>body{mso-number-format:\"\\@\";}</style></head>");
+        Response.Write("<body>");
+        Response.Write("<table>");
+
+        // Agregar encabezados
+        Response.Write("<tr>");
+        Response.Write("<td>ID</td>");
+        Response.Write("<td>Fecha de Factura</td>");
+        Response.Write("<td>CIF del Cliente</td>");
+        Response.Write("<td>Nombre del Cliente</td>");
+        Response.Write("<td>Importe</td>");
+        Response.Write("<td>Importe + IVA</td>");
+        Response.Write("<td>Moneda</td>");
+        Response.Write("<td>Fecha de Cobro</td>");
+        Response.Write("<td>Estado</td>");
+        Response.Write("</tr>");
+
+        // Agregar datos de facturas al archivo XLS
+        foreach (GridViewRow row in GridView1.Rows)
+        {
+            Response.Write("<tr>");
+            Response.Write("<td>" + row.Cells[0].Text + "</td>");
+            Response.Write("<td>" + row.Cells[1].Text + "</td>");
+            Response.Write("<td>" + row.Cells[2].Text + "</td>");
+            Response.Write("<td>" + row.Cells[3].Text + "</td>");
+            Response.Write("<td>" + row.Cells[4].Text + "</td>");
+            Response.Write("<td>" + row.Cells[5].Text + "</td>");
+            Response.Write("<td>" + row.Cells[6].Text + "</td>");
+            Response.Write("<td>" + row.Cells[7].Text + "</td>");
+            Response.Write("<td>" + row.Cells[8].Text + "</td>");
+            Response.Write("</tr>");
+        }
+
+        Response.Write("</table>");
+        Response.Write("</body>");
+        Response.Write("</html>");
+
+        // Finalizar la respuesta para descargar el archivo
+        Response.End();
     }
 }
